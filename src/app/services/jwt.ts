8aa9559eb11ee9296af522d401d1fa2887b6aba9
Shashi <shashirajt20@gmail.com>
2025-10-17 10:03:57 +0530

    fixed dynamic route type error

--- src/app/services/jwt.ts
+++ src/app/services/jwt.ts
@@ -1,13 +1,19 @@
 import * as jwt from "jsonwebtoken";
 
-export function generateToken(data){
+export function generateToken(data:{id:string}){
     //@ts-ignore
     const token = jwt.sign(data, process.env.JWT_SECRET as string)
     return token;
 }
 
-export function verifyToken(token){
+export function verifyToken(token :string):{id:string}| null{
     //@ts-ignore
-    const data = jwt.verify(token, process.env.JWT_SECRET)
+    // const data = jwt.verify(token, process.env.JWT_SECRET)
+    // return data;
+    try {
+    const data = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
     return data;
+  } catch (error) {
+    return null; // agar invalid ya expired token hai
+  }
 }
